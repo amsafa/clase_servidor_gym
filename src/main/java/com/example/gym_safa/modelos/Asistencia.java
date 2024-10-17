9c31f11621faa2b96050ac49a0a6d0a60de03e10
avidalsafa <avidalgalan@safareyes.es>
2024-10-17 18:47:54 +0200

    Tengo que hacer el resto de métodos.

--- src/main/java/com/example/gym_safa/modelos/Asistencia.java
+++ src/main/java/com/example/gym_safa/modelos/Asistencia.java
@@ -1,5 +1,6 @@
 package com.example.gym_safa.modelos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Asistencia {
 
     @Id
--- src/main/java/com/example/gym_safa/repositorios/AsistenciaRepository.java
+++ src/main/java/com/example/gym_safa/repositorios/AsistenciaRepository.java
@@ -5,6 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.gym_safa.modelos.Asistencia;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AsistenciaRepository extends JpaRepository<Asistencia, Integer> {
+
+    // Tengo que rehacerlo junto al metodo getAsistenciaResumenBySocioId
+    List<Asistencia> findBySocioId(Integer socioId);
 }
