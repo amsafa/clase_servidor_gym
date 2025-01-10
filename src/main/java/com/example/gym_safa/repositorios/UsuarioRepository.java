e35ce60ca25c60911ecc7b2b641098433bf07831
alba <sfg@sfg.es>
2025-01-10 20:32:58 +0100

    Borrar UsuarioRepository.

--- src/main/java/com/example/gym_safa/repositorios/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gym_safa.repositorios;
-
-import com.example.gym_safa.modelos.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<Usuario> findByUsername(String username);
-}
